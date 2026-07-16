ae3fa7c27a6819acfe9786e55a1d241cef8f4f8c
Cristofer Sousa <cristofer.sousa@sinch.com>
2026-07-16 12:19:30 -0300

    feat: change file and imported components layout files

--- vue3-template-info2/src/App.vue
+++ vue3-template-info2/src/App.vue
@@ -1,20 +1,26 @@
 <template>
+ <AppHeader />
   <main class="main">
        <RouterView />
   </main>
+<AppFooter />
+
 </template>
 
 <script setup>
 import {RouterView } from 'vue-router'
+import AppHeader from './components/layout/AppHeader.vue';
+import AppFooter from './components/layout/AppFooter.vue';
 </script>
 
 <style scoped>
 .main {
+  padding: 10px;
   background: #fafafa;
   color: #333;
   width: 1200px;
   margin: 0 auto;
-  min-height: 100vh;
+  min-height: 60vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
