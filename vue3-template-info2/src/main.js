e74cf5b7581c9f69426e45235dcfe21ee9d336b9
Cristofer Sousa <cristofer.sousa@sinch.com>
2026-07-16 12:18:11 -0300

    feat: added font-awesome and configured main

--- vue3-template-info2/src/main.js
+++ vue3-template-info2/src/main.js
@@ -3,6 +3,7 @@ import '@/assets/main.css'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import '@fortawesome/fontawesome-free/js/all.js' //https://docs.fontawesome.com/
 
 const app = createApp(App)
 app.use(router)
